e27434afb8df9971e989388dda687810fb08f01a
akxaalex16 <akxaalex16@gmail.com>
2024-06-04 20:28:36 -0700

    Variables

---
+++ 1-variables.py
@@ -0,0 +1,26 @@
+# integer
+price=10
+# changed value of price
+price=20 
+
+# float
+rating=3.9
+
+# string
+name="bean"
+
+# boolean
+is_published= True
+
+# displayed in terminal
+print(price)
+print(rating)
+print(name)
+print(is_published)
+
+# example: we check in a patient named John Smith, 
+# he's 20 years old and is a new patient
+
+name1="John Smith"
+age=20
+new_patient= True
