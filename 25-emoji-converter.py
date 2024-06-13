57fcf4d68d642d70f85478a40ca5bf1e020c9812
akxaalex16 <akxaalex16@gmail.com>
2024-06-12 21:47:29 -0700

    added emoji-converter.py
    emoji converter challenge using dictionaries

---
+++ 25-emoji-converter.py
@@ -0,0 +1,12 @@
+message = input("> ")
+words = message.split(" ")
+
+emojis =  {
+    ":(" : "😞",
+    ":)" : "😊"
+           }
+
+output= ""
+for item in words:
+    output += emojis.get(item, item) + " "
+print(output)
